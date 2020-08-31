fa46e8ae127d1b46ab93d8fa136f08983dc36ad9
Babak Behzad <babakbehzad@gmail.com>
2020-08-31 16:55:13 -0700

    Update the destroy script

--- destroy_druid_cluster.sh
+++ destroy_druid_cluster.sh
@@ -1,4 +1,6 @@
 kubectl delete -f examples/prod-cluster.yaml
-kubectl exec zookeeper-0 -n zookeeper zkCli.sh rmr /druid
-gcloud sql databases delete druid --instance=intellipse-metadata
+gcloud -q sql databases delete druid --instance=intellipse-metadata
 kubectl delete pvc --all -n druid
+gsutil -q rm -r gs://production-205919/druid
+sleep 5
+kubectl exec zookeeper-0 -n zookeeper zkCli.sh rmr /druid
